0f86489a267caa22bf40f2568b320d27b36dcbe0
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-06-07 16:33:15 +0200

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -43,9 +43,35 @@
 # Copyright 2018 Your name here, unless otherwise noted.
 #
 class role_geneious (
+  $compose_version     = '1.19.0',
+  $MYSQL_VERSION       = '5.5.60'
   $MYSQL_ROOT_PASSWORD = 'changeme',
-  $MYSQL_DATABASE      = 'naturalis-geneious',
+  $MYSQL_DATABASE      = 'naturalis-geneious'
   ) {
 
+  # Install docker
+  class {'docker':
+  }
+
+  # Install docker compose
+  class {'docker::compose':
+    version => $role_geneious::compose_version
+  }
+
+  # Download docker repo
+  vcsrepo { '/opt/docker-geneious':
+    ensure   => present,
+    provider => git,
+    source   => 'https://github.com/naturalis/docker-geneious.git'
+    #require  => Package['git'],
+  }
+
+  # Start containers
+  docker_compose { "/opt/docker-geneious/docker-compose.yml":
+    ensure  => present,
+    require => [
+      Vcsrepo['/opt/docker-geneious']
+    ]
+  }
 
 }
