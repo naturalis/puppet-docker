63d4768602547b38fa3238fbd3f6aff999278afb
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2018-06-14 12:09:52 +0200

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -44,6 +44,8 @@
 #
 class role_geneious (
   $compose_version     = '1.19.0',
+  $docker_repo_dir     = '/opt/docker-geneious',
+  $docker_repo_source  = 'https://github.com/naturalis/docker-geneious.git'
   $mysql_version       = '5.5.60',
   $mysql_root_password = 'changeme',
   $mysql_database      = 'naturalis-geneious'
@@ -55,22 +57,30 @@ class role_geneious (
 
   # Install docker compose
   class {'docker::compose':
-    version => $role_geneious::compose_version
+    version => $::compose_version
   }
 
   # Download docker repo
-  vcsrepo { '/opt/docker-geneious':
+  vcsrepo { $::docker_repo_dir:
     ensure   => present,
     provider => git,
-    source   => 'https://github.com/naturalis/docker-geneious.git'
+    source   => $::docker_repo_source,
     #require  => Package['git'],
   }
 
+  # Replace .env file
+  file { "${::repo_dir}/.env":
+    ensure   => file,
+    content  => template('role_dockertreebase/prod.env.erb'),
+    require  => Vcsrepo[$::docker_repo_dir],
+    #notify   => Exec['Restart containers on change'],
+  }
+
   # Start containers
-  docker_compose { "/opt/docker-geneious/docker-compose.yml":
+  docker_compose { "{$::repo_dir}/docker-compose.yml":
     ensure  => present,
     require => [
-      Vcsrepo['/opt/docker-geneious']
+      Vcsrepo[$::docker_repo_dir]
     ]
   }
 
